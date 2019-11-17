e8c56b99267a2a94711d5a10f656291dec4e3af5
dinoshan <dinoshan@buffalo.edu>
2019-11-16 20:00:36 -0500

    Updates

---
+++ precache-manifest.991e3e437b55cd780200a1d94f071471.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0167892d68f98a2e0760179895dc97a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b3abc08e2994ae6ec14",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "64451167eb52c448cb87",
+    "url": "/static/css/main.f755afe4.chunk.css"
+  },
+  {
+    "revision": "5b3abc08e2994ae6ec14",
+    "url": "/static/js/2.8e1d5428.chunk.js"
+  },
+  {
+    "revision": "64451167eb52c448cb87",
+    "url": "/static/js/main.5f10c461.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "afab80e86b4026fff42fa5341e513040",
+    "url": "/static/media/devgrub.afab80e8.png"
+  },
+  {
+    "revision": "a88dcd6f036f4a0cc59c39f11bed573c",
+    "url": "/static/media/evverest.a88dcd6f.png"
+  },
+  {
+    "revision": "ce30560c0c0b96652159cb91837c48f8",
+    "url": "/static/media/youtube.ce30560c.png"
+  }
+]);
